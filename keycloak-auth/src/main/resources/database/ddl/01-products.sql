0c72e946fbbbbd040d060365cbbf2f650071db35
joalvarez <alvarez.joaquinez@gmail.com>
2024-02-17 14:17:16 -0300

    feat: add sql file to create product table

--- keycloak-auth/src/main/resources/database/ddl/01-products.sql
+++ keycloak-auth/src/main/resources/database/ddl/01-products.sql
@@ -1,6 +1,15 @@
-CREATE TABLE IF NOT EXISTS products (
-    id SERIAL NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    price DOUBLE PRECISION NOT NULL,
-    CONSTRAINT "pk_products" PRIMARY KEY (id)
-);
+DO $$
+  DECLARE
+    r RECORD;
+  BEGIN
+    FOR r IN SELECT schema_name
+             FROM information_schema.schemata
+             WHERE schema_name NOT IN ('information_schema', 'pg_catalog', 'pg_toast')
+      LOOP
+        EXECUTE 'CREATE TABLE IF NOT EXISTS ' || r.schema_name || '.products (' ||
+                'id SERIAL PRIMARY KEY,' ||
+                'name VARCHAR(50) NOT NULL,' ||
+                'price DOUBLE PRECISION NOT NULL);';
+      END LOOP;
+END$$;
+;
